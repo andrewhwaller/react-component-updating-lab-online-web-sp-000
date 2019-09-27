0c6cf8c42ef9e2ced10f6f85c9905dd8942f53be
Andrew Waller <andrewhwaller@gmail.com>
2019-09-27 18:30:27 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -14,6 +14,10 @@ class Timer extends Component {
     this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
 
+  componentShouldUpdate() {
+
+  }
+
   componentDidMount() {
     this.interval = setInterval(
       this.clockTick,
